ef32ae568f71cf67ee4fa86b185282acf3d6c3de
AliyarP <aliarcomand4blue@gmail.com>
2025-07-13 20:53:26 +0500

    тadsaasxест

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -21,7 +21,7 @@ export default function Main() {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
       >
-        <source src="/video.mp4" type="video/mp4" />
+        <source src="../video.mp4" type="video/mp4" />
         Ваш браузер не поддерживает видео.
       </video>
 
